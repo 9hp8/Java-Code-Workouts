6552453f1effd416fd3a63635d39a31b4ce576ed
9hp8 <sri13haripriya@gmail.com>
2024-08-29 19:42:08 -0500

    Code for difference in strings

---
+++ src/java_coding_string/DifferenceInStrings.java
@@ -0,0 +1,27 @@
+package java_coding_string;
+
+import java.util.Scanner;
+public class DifferenceInStrings {
+
+	public char findTheDifference(String s, String t) {
+		int result = 0;
+		for(int i=0;i<s.length();i++) {
+			result ^= s.charAt(i);
+		}
+		for(int i=0;i<t.length();i++) {
+				result ^= t.charAt(i); 	
+				}
+		return (char)result;
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine(); // abcd
+		String t = scan.nextLine(); // abcde
+		
+		DifferenceInStrings differenceInStrings = new DifferenceInStrings();
+		char result = differenceInStrings.findTheDifference(s, t);
+		System.out.println(result);
+		
+	}
+
+}
